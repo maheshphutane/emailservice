12155ea45d4aa3dab2f22dcdcd216e6c39f3849d
Mahesh <maheshphutane1810@gmail.com>
2024-05-14 14:39:06 +0530

    Initializing EmailService

---
+++ src/test/java/com/example/emailserviceapi/EmailServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emailserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
